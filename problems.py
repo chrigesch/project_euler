254ce39d3895f037b9b9134278a171b4cbc0da3f
chrigesch <christianschetsche@psi.uba.ar>
2025-04-14 17:21:59 -0300

    feat: problem_0019

--- problems.py
+++ problems.py
@@ -130,7 +130,7 @@ def problem_0011(matrix: pd.DataFrame, n: int) -> int:
         for c in range(len(matrix)):
             # right & left
             if c < len(matrix) - n:
-                largest_prod_l_r = matrix.iloc[r, c : c + n].prod()  # noqa E203
+                largest_prod_l_r = matrix.iloc[r, c : c + n].prod()  # type: ignore # noqa E203
             # up & down
             if r < len(matrix) - n:
                 largest_prod_u_d = matrix.iloc[r : r + n, c].prod()  # noqa E203
@@ -247,6 +247,64 @@ def problem_0018() -> int:
             dp[line][e] = tri[line][e] + max(dp[line - 1][e - 1], dp[line - 1][e])
 
     return max(dp[-1])
+
+
+def problem_0019() -> int:
+    date = {
+        "day": 1,
+        "month": 1,
+        "year": 1900,
+        "weekday": 1,
+    }
+    counter = 0
+    while date["year"] <= 2000:
+        if date["month"] in [4, 6, 9, 11]:
+            if date["day"] == 30:
+                date["day"] = 1
+                date["month"] += 1
+            else:
+                date["day"] += 1
+        elif date["month"] in [1, 3, 5, 7, 8, 10, 12]:
+            if date["day"] == 31:
+                date["day"] = 1
+                if date["month"] == 12:
+                    date["month"] = 1
+                    date["year"] += 1
+                else:
+                    date["month"] += 1
+            else:
+                date["day"] += 1
+        else:
+            if date["year"] % 4 == 0:
+                if ((date["year"] % 100 == 0) & (date["year"] % 400 == 0)) | (
+                    date["year"] % 100 != 0
+                ):
+                    if date["day"] == 29:
+                        date["day"] = 1
+                        date["month"] += 1
+                    else:
+                        date["day"] += 1
+                else:
+                    if date["day"] == 28:
+                        date["day"] = 1
+                        date["month"] += 1
+                    else:
+                        date["day"] += 1
+            else:
+                if date["day"] == 28:
+                    date["day"] = 1
+                    date["month"] += 1
+                else:
+                    date["day"] += 1
+
+        if date["weekday"] == 7:
+            date["weekday"] = 1
+        else:
+            date["weekday"] += 1
+
+        if (date["day"] == 1) & (date["weekday"] == 7) & (date["year"] >= 1901):
+            counter += 1
+    return counter
 
 
 def problem_0027() -> list:
